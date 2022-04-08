832b305a8c26d2b13d9ca52e0a315a63c64c6bf4
vis98fl <fllap0257@FLLAP0257s-MacBook-Air.local>
2022-04-08 14:15:41 +0530

    added multiple values using insert

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const res = require("express/lib/response");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -15,13 +16,21 @@ connection.connect((err) => {
       //sql="CREATE TABLE students (id INT, name VARCHAR(255), course VARCHAR(255))"
       //sql=sql = "INSERT INTO students (id, name, course) VALUES (1, 'John Doe', 'Computer Science')";
       //var sql = "SELECT * FROM students";
-      var sql = "UPDATE students SET course = 'Art' WHERE id = 1";
-
-      connection.query(sql, function (err, result) {
+      //var sql = "UPDATE students SET course = 'Art' WHERE id = 1";
+//multiple inserts belows
+var st=new Date().getTime;
+var sql="INSERT INTO students (id, name, course) VALUES?"
+var values=[['2','raj','hist'],['3','ram','geo'],
+['4','raju','sci'],['5','rakesh','maths'],['6','hitesh','sci']]      
+connection.query(sql,[values], function (err, result) {
         if (err) {
           console.log(err);
         }
-        console.log("updated");
+        //console.log("updated");
+        console.log("added");
+       var ed=new Date().getTime;
+       console.log("total",(ed-st)/60);
+       console.log("added",result.affectedRows);
       });
 
     }
